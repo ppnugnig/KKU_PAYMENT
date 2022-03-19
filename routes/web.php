cb1c4b575158d78b4577a7a1c1ed663c99a7a45f
nutchaya2001 <nutchaya.pr@kkumail.com>
2022-03-19 17:07:28 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,5 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('admin/home', [HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
+
+Route::resource('schedules',ScheduleController::class);
