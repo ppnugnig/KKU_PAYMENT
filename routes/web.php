c3c4c421ce074d89db97f0f27448313da14dbc0c
PPNUGNIG <pattamaporn.wo@kkumail.com>
2022-03-21 17:56:48 +0700

    nug push

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/', function () {
 });
 
 // Auth::routes();
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/account',[AccountController::class,'account']);
 Route::get('/navbar',[NavbarController::class,'navbar']);
